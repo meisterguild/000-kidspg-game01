7f2671388b79177c272ece401bc97e61f23d7767
MGHiOgawa <hi.ogawa@m-gild.com>
2025-08-28 14:10:00 +0900

    add memorial card images(split 5 bin files)

---
+++ docs/memorial_site_src/split/card_images_join.bat
@@ -0,0 +1,5 @@
+@echo off
+echo Œ‹‡ˆ—‚ğŠJn‚µ‚Ü‚·...
+copy /b card_images.part000 + card_images.part001 + card_images.part002 + card_images.part003 + card_images.part004 card_images.zip
+echo Š®—¹‚µ‚Ü‚µ‚½ ¨ card_images.zip
+pause
