250bf0fc20e8c7114961345453ec7002eb3ecd1d
MGHiOgawa <hi.ogawa@m-gild.com>
2025-08-28 14:19:36 +0900

    fix memorial card images(split 9 bin files)

--- docs/memorial_site_src/split/card_images_join.bat
+++ docs/memorial_site_src/split/card_images_join.bat
@@ -1,5 +1,5 @@
 @echo off
 echo Œ‹‡ˆ—‚ğŠJn‚µ‚Ü‚·...
-copy /b card_images.part000 + card_images.part001 + card_images.part002 + card_images.part003 + card_images.part004 card_images.zip
+copy /b card_images.part000 + card_images.part001 + card_images.part002 + card_images.part003 + card_images.part004 + card_images.part005 + card_images.part006 + card_images.part007 + card_images.part008 card_images.zip
 echo Š®—¹‚µ‚Ü‚µ‚½ ¨ card_images.zip
 pause
